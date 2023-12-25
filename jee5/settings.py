349f78acbc7cd00955828c1770d9a0a5643d5029
Swadhin Maharana <115407035+cursorhigh@users.noreply.github.com>
2023-12-25 11:54:57 +0530

    Update settings.py

--- jee5/settings.py
+++ jee5/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*i$at@th)ga^jrl@81jh__4dx3i6=e#ob=*u9ozt2hcl%gmx_b'
+SECRET_KEY = "Idk bro"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
